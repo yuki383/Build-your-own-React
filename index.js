4619555ff7b5b0e661e00805486ded3fd3821451
yuu383 <yuukikokubo.pg@gmail.com>
2020-04-18 16:55:20 +0900

    feat: render function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function createElement(type, props, ...children) {
     props: {
       ...props,
       children: children.map((child) =>
-        typeof child === "object" ? child : createTextElement(text)
+        typeof child === "object" ? child : createTextElement(child)
       ),
     },
   };
@@ -20,8 +20,26 @@ function createTextElement(text) {
   };
 }
 
+function render(element, container) {
+  const dom =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+
+  const isProperty = (key) => key !== "children";
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach((name) => {
+      dom[name] = element.props[name];
+    });
+
+  element.props.children.forEach((child) => render(child, dom));
+  container.appendChild(dom);
+}
+
 const Didact = {
   createElement,
+  render,
 };
 
 /** @jsx Didact.createElement */
@@ -33,3 +51,4 @@ const element = (
 );
 
 const container = document.getElementById("root");
+Didact.render(element, container);
